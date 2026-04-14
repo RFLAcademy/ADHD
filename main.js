c91e843eebb4c7529f1ed47e7690ddcd2c04bed5
RFL Academy <competitions.mum@robofunlab.com>
2026-04-14 17:59:18 +0530

    fix build config

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1000,
-    height: 700,
+    width: 800,
+    height: 600
   });
 
   win.loadFile('index.html');
